6f386d591b04c7e802164bfae83ff88cd9f8ed34
unknown <bazo20061996@gmail.com>
2022-08-01 15:30:06 +0700

    up1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Circle circle1 = new Circle(5, "blue");
-        circle1.toString();
-        System.out.println("dien tich hinh tron 1: " + circle1.getArea());
+        Circle circle1 = new Circle();
+        circle1.setRadius(5);
+        circle1.setColor("blue");
+        System.out.println("dien tich hinh tron 1: " + circle1);
         Cylinder cylinder1 = new Cylinder(circle1.getRadius(), circle1.getColor(), 20);
-        System.out.println(circle1);
         System.out.println("the tich khoi1: "+ cylinder1);
 
     }
